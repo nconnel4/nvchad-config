f86279897ad0a60d5a6c0691961d435d803f4947
nconnel4 <nconnel4@hotmail.com>
2023-03-22 17:22:33 -0400

    Add custom nvchad config

---
+++ custom/configs/overrides.lua
@@ -0,0 +1,63 @@
+local M = {}
+
+M.treesitter = {
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"python",
+	},
+	indent = {
+		enable = true,
+		disable = {
+			"python",
+		},
+	},
+}
+
+M.mason = {
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
+
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+
+		-- python
+		"pyright",
+		"black",
+		"isort",
+		"mypy",
+		"flake8",
+
+		-- python-django
+		"djlint",
+	},
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+	git = {
+		enable = true,
+	},
+
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+}
+
+return M
